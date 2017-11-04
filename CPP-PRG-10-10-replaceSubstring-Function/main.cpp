5ac5076c4c7ccbbad0e72a36286ff9450851b658
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-04 08:41:50 -0400

    Prompt written.

--- CPP-PRG-10-10-replaceSubstring-Function/main.cpp
+++ CPP-PRG-10-10-replaceSubstring-Function/main.cpp
@@ -5,11 +5,27 @@
 //  Created by Keith Smith on 11/4/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a function named replaceSubstring. The function should accept three C-string
+//  of string object arguments. Let's call them string1, string2, and string3. It
+//  should search string1 for all occurences of string2. When it finds an occurence of
+//  string2, it should replace it with string3. For eample, suppose the three arguments
+//  have the following values:
+//
+//      string1:        "the dog jumped over the fence"
+//      string2:        "the"
+//      string3:        "that"
+//
+//  With these three arguments, the function would return a string object with the value
+//  "that dog jumped over the fence." Demonstrate the function in a complete program.
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
+    
     return 0;
 }
